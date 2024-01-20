a85633e6879336b2bae1808e8184d781c169e9b3
Celestin Niyonsaba <niyoceles3@gmail.com>
2024-01-20 15:42:47 +0200

    Update backend3

--- services/payment.service.js
+++ services/payment.service.js
@@ -1,5 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-const axios = require('axios');
+// const axios = require('axios');
 const {
   generateUUID
 } = require('../utils/uui');
@@ -9,38 +11,38 @@ const {
 } = require('../models');
 
 const ROOT_MASTERCARD_API = 'https://ap-gateway.mastercard.com/api/rest/version/61/merchant/8206000697';
-const createPaymentSession = async (amount) => {
-  const data = {
-    apiOperation: 'CREATE_CHECKOUT_SESSION',
-    interaction: {
-      operation: 'PURCHASE',
-    },
-    order: {
-      amount: parseFloat(amount),
-      currency: 'USD',
-      id: `${generateUUID()}`,
-    },
-  };
+// const createPaymentSession = async (amount) => {
+//   const data = {
+//     apiOperation: 'CREATE_CHECKOUT_SESSION',
+//     interaction: {
+//       operation: 'PURCHASE',
+//     },
+//     order: {
+//       amount: parseFloat(amount),
+//       currency: 'USD',
+//       id: `${generateUUID()}`,
+//     },
+//   };
 
-  const response = await axios.post(`${ROOT_MASTERCARD_API}/session`, data, {
-    headers: {
-      Authorization: 'Basic bWVyY2hhbnQuODIwNjAwMDY5NzpiZTcxZTIyYmM1YTZjMWQ0YmRhYWE4NWY3OWM2NTBiZA==',
-      'Content-Type': 'application/json',
-    },
-  });
+//   const response = await axios.post(`${ROOT_MASTERCARD_API}/session`, data, {
+//     headers: {
+//       Authorization: 'Basic bWVyY2hhbnQuODIwNjAwMDY5NzpiZTcxZTIyYmM1YTZjMWQ0YmRhYWE4NWY3OWM2NTBiZA==',
+//       'Content-Type': 'application/json',
+//     },
+//   });
 
-  return response;
-};
+//   return response;
+// };
 
-const getTransactionResults = async (orderId) => {
-  const response = await axios.get(`${ROOT_MASTERCARD_API}/order/${orderId}`, {
-    headers: {
-      Authorization: 'Basic bWVyY2hhbnQuODIwNjAwMDY5NzpiZTcxZTIyYmM1YTZjMWQ0YmRhYWE4NWY3OWM2NTBiZA==',
-      'Content-Type': 'application/json',
-    },
-  });
-  return response;
-};
+// const getTransactionResults = async (orderId) => {
+//   const response = await axios.get(`${ROOT_MASTERCARD_API}/order/${orderId}`, {
+//     headers: {
+//       Authorization: 'Basic bWVyY2hhbnQuODIwNjAwMDY5NzpiZTcxZTIyYmM1YTZjMWQ0YmRhYWE4NWY3OWM2NTBiZA==',
+//       'Content-Type': 'application/json',
+//     },
+//   });
+//   return response;
+// };
 const createPayment = async (paymentBody) => {
   const {
     name, email, amount, status
@@ -66,8 +68,8 @@ const getPaymentById = async (id) => Payment.findById(id);
 
 module.exports = {
   createPayment,
-  createPaymentSession,
+  // createPaymentSession,
   queryPayments,
-  getTransactionResults,
+  // getTransactionResults,
   getPaymentById,
 };
